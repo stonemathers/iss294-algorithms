9f472d226ab7016e5cd13d23414d9ba499eb253f
Stone Mathers <smm117@duke.edu>
2019-02-13 20:48:53 -0500

    Added color pallette, dragability, font, and changed blend mode

--- sketch.js
+++ sketch.js
@@ -4,26 +4,43 @@ const BG_COLOR = "#000";
 //Fractal vars
 let fractals = [];
 let fractalColor;
+let clickedFractal;
 const FRACT_DIV = 2.5; //Amount to divide by at each fractal step
 const INIT_RAD = 10;
 const MIN_RAD = 2;
 const RAD_INC = 3;
+const MAX_SIZE = 240;
 
 //Color Pallette
-const COLOR_PALLETE = ["#00f", "#0f0", "#f00", "#0ff", "#f0f", "#0ff"];
+const COLOR_PALLETE = ["#f66", "#f99", "#fcc", "#ffb266", "#fc9", "#ffe5cc",
+                        "#ff6", "#ff9", "#ffc", "#b2ff66", "#cf9", "#e5ffcc",
+                        "#6f6", "#9f9", "#cfc", "#66ffb2", "#9fc", "#ccffe5",
+                        "#6ff", "#9ff", "#cff", "#66b2ff", "#9cf", "#cce5ff",
+                        "#66f", "#99f", "#ccf", "#b266ff", "#c9f", "#e5ccff",
+                        "#f6f", "#f9f", "#fcf", "#ff66b2", "#f9c", "#ffcce5"];
 
 //Initial state boolean
 let drawingStarted;
 
+//Mode variable (After drawing starts)
+const MODE_DRAW = 0;
+const MODE_DRAG = 1;
+const MODE_DISPLAY = 2;
+let mode = MODE_DISPLAY;
+
+//Drag vars
+const DRAG_SPACING = 5;
+let dragCounter = DRAG_SPACING;
+
 //Start message vars
 const START_MESSAGE_TEXT = "Click to draw...";
-//let startMessageFont;
+let startMessageFont;
 
 /*
 * Run once before site loads
 */
 function preload(){
-    //startMessageFont = loadFont("fonts/Major_Mono_Display/MajorMonoDisplay-Regular.ttf");
+    startMessageFont = loadFont("assets/fonts/Monoton/Monoton-Regular.ttf");
 }
 
 /*
@@ -32,7 +49,7 @@ function preload(){
 function setup(){
     createCanvas(windowWidth, windowHeight);
     drawingStarted = false;
-    //startMessageFont = font_arr[getRandomInt(0, font_arr.length - 1)];
+    mode = MODE_DRAW;
     background(BG_COLOR);
     displayStartMessage();
 }
@@ -43,17 +60,19 @@ function setup(){
 function draw(){
     if(drawingStarted){
         //Draw fractals
+        blendMode(DIFFERENCE);
         for(i = 0; i < fractals.length; i++){
             let currFrac = fractals[i];
-            if(i == fractals.length - 1 && mouseIsPressed){
-                currFrac.increaseSize(RAD_INC);
+            if(i == fractals.length - 1 && mode == MODE_DRAW){
+                if(currFrac.getSize() < MAX_SIZE){
+                    currFrac.increaseSize(RAD_INC);
+                }
                 currFrac.display();
             }else{
                 if(currFrac.stillDisplaying()){
                     currFrac.displayNextStep();
                 }else{
-                    fractals.splice(i, 1);
-                    i--;
+                    //Do nothing
                 }
             }
         }
@@ -75,8 +94,8 @@ function windowResized() {
 */
 function keyPressed(){
     if(key == 'r' || key == 'R'){
-        blendMode(BLEND);
         fractals = [];
+        blendMode(BLEND);
         background(BG_COLOR);
     }
 }
@@ -89,9 +108,42 @@ function mousePressed(){
         drawingStarted = true;
         background(BG_COLOR);
     }
-    //fractalColor = color(COLOR_PALLETE[Math.floor(random(COLOR_PALLETE.length))]);
-    fractalColor = color(random(255), random(255), random(255));
-    fractals.push(new Fractal(mouseX, mouseY, INIT_RAD, fractalColor));
+
+    clickedFractal = getClosestClickedFractal();
+
+    if(clickedFractal !== null){
+        mode = MODE_DRAG;
+        dragCounter = DRAG_SPACING;
+        blendMode(BLEND);
+        clickedFractal.display();
+    }else{
+        mode = MODE_DRAW;
+        fractalColor = color(COLOR_PALLETE[Math.floor(random(COLOR_PALLETE.length))]);
+        fractals.push(new Fractal(mouseX, mouseY, INIT_RAD, fractalColor));
+    }
+}
+
+/*
+* Run when mouse is dragged
+*/
+function mouseDragged(){
+    if(mode == MODE_DRAG){
+        dragCounter--;
+        if(dragCounter == 0){
+            clickedFractal.x = mouseX;
+            clickedFractal.y = mouseY;
+            blendMode(DIFFERENCE);
+            clickedFractal.display();
+            dragCounter = DRAG_SPACING;
+        }
+    }
+}
+
+/*
+* Run when mouse is dragged
+*/
+function mouseReleased(){
+    mode = MODE_DISPLAY;
 }
 
 /*
@@ -105,8 +157,30 @@ function displayStartMessage(){
     fill("white");
     textSize(size);
     textAlign(CENTER, CENTER);
-    //textFont(startMessageFont);
+    textFont(startMessageFont);
     text(START_MESSAGE_TEXT, x, y);
+}
+
+/*
+* Return the Fractal that was clicked. If multiple fractals were clicked,
+* return the one whose center is closest to the mouse.
+*/
+function getClosestClickedFractal(){
+    let closest = null;
+    let minDist = Number.MAX_VALUE;
+
+    //Iterate from end of array - if there's a tie we want the most recent one
+    for(let i = fractals.length - 1; i >= 0; i--){
+        let currFrac = fractals[i];
+        if(currFrac.mouseIsOver()){
+            let dist = currFrac.distFromMouse();
+            if(dist < minDist){
+                closest = currFrac;
+            }
+        }
+    }
+
+    return closest;
 }
 
 /*
@@ -122,10 +196,10 @@ class Fractal{
     }
 
     /*
-    * Determine whether fractal has more steps to display
+    * Determine whether Fractal has more steps to display
     *
     * Output:
-    *   true if fractal has more steps to display, false if not
+    *   true if Fractal has more steps to display, false if not
     */
     stillDisplaying(){
         let denom = Math.pow(FRACT_DIV, this.stepsToDisplay - 1);
@@ -133,14 +207,12 @@ class Fractal{
     }
 
     /*
-    * Display fractal up to current step
+    * Display Fractal up to current step
     */
     display(){
         fill(this.color);
         strokeWeight(0);
-        blendMode(LIGHTEST);
         this.fractalRecurse(this.x, this.y, this.startSize, this.stepsToDisplay);
-        //ellipse(this.x, this.y, this.startSize, this.startSize);
     }
 
     /*
@@ -166,7 +238,7 @@ class Fractal{
     }
 
     /*
-    * Increment current step and then display fractal
+    * Increment current step and then display Fractal
     */
     displayNextStep(){
         this.stepsToDisplay++;
@@ -174,7 +246,14 @@ class Fractal{
     }
 
     /*
-    * Increase fractal starting size
+    * Get the Fractal's starting size (diameter of initial circle)
+    */
+    getSize(){
+        return this.startSize;
+    }
+
+    /*
+    * Increase Fractal starting size
     *
     * Input:
     *   amount - integer amount to increase start size
@@ -182,4 +261,19 @@ class Fractal{
     increaseSize(amount){
         this.startSize += amount;
     }
+
+    /*
+    * Return true if mouse is over initial Fractal circle, false if not
+    */
+    mouseIsOver(){
+        return this.distFromMouse() < this.startSize/2;
+    }
+
+    /*
+    * Calculate and return distance between mouse and center of Fractal
+    */
+    distFromMouse(){
+        return dist(mouseX, mouseY, this.x, this.y);
+    }
+
 }
